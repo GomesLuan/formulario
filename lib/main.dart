74e0fc9f66544168969af62f356490609e0ebffd
Luan Victor <luan.araujo.gom@gmail.com>
2023-04-23 23:14:05 -0300

    Botão de finalizar formulário e formatações

--- lib/main.dart
+++ lib/main.dart
@@ -37,107 +37,129 @@ class MyFormWidgetState extends State<MyFormWidget> {
   Widget build(BuildContext context) {
     return Form(
       key: formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Nome:',
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Por favor, digite algo';
-              }
-              return null;
-            }
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Telefone:',
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Por favor, digite algo';
-              }
-              return null;
-            }
-          ),
-          Text('Idade'),
-          Slider(
-            value: currentSliderValue,
-            min: 18,
-            max: 100,
-            divisions: 82,
-            label: currentSliderValue.round().toString(),
-            onChanged: (double value) {
-              setState(() {
-                currentSliderValue = value;
-              });
-            },
-          ),
-          Text('Monte seu almoço'),
-          CheckboxListTile(
-            title: Text('Arroz'),
-            value: checkBoxesValues[0], 
-            onChanged: (bool? value) {
-              setState(() {
-                checkBoxesValues[0] = value;
-              });
-            },
-            controlAffinity: ListTileControlAffinity.leading,
-          ),
-          CheckboxListTile(
-            title: Text('Feijão'),
-            value: checkBoxesValues[1], 
-            onChanged: (bool? value) {
-              setState(() {
-                checkBoxesValues[1] = value;
-              });
-            },
-            controlAffinity: ListTileControlAffinity.leading,
-          ),
-          CheckboxListTile(
-            title: Text('Frango'),
-            value: checkBoxesValues[2], 
-            onChanged: (bool? value) {
-              setState(() {
-                checkBoxesValues[2] = value;
-              });
-            },
-            controlAffinity: ListTileControlAffinity.leading,
-          ),
-          CheckboxListTile(
-            title: Text('Salada'),
-            value: checkBoxesValues[3], 
-            onChanged: (bool? value) {
-              setState(() {
-                checkBoxesValues[3] = value;
-              });
-            },
-            controlAffinity: ListTileControlAffinity.leading,
-          ),
-          Text('Vai querer sobremesa?'),
-          RadioListTile(
-            title: Text('Sim'),
-            value: RadioBoxOptions.sim, 
-            groupValue: radioBoxGroup, 
-            onChanged: (RadioBoxOptions? value) {
-              setState(() {
-                radioBoxGroup = value;
-              });
-            },
-          ),
-          RadioListTile(
-            title: Text('Não'),
-            value: RadioBoxOptions.nao, 
-            groupValue: radioBoxGroup, 
-            onChanged: (RadioBoxOptions? value) {
-              setState(() {
-                radioBoxGroup = value;
-              });
-            },
+      child: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child:Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Nome:',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, digite algo';
+                  }
+                  return null;
+                }
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Telefone:',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, digite algo';
+                  }
+                  return null;
+                }
+              ),
+              SizedBox(height: 10),
+              Text('Idade'),
+              Slider(
+                value: currentSliderValue,
+                min: 18,
+                max: 100,
+                divisions: 82,
+                label: currentSliderValue.round().toString(),
+                onChanged: (double value) {
+                  setState(() {
+                    currentSliderValue = value;
+                  });
+                },
+              ),
+              SizedBox(height: 10),
+              Text('Monte seu almoço'),
+              CheckboxListTile(
+                title: Text('Arroz'),
+                value: checkBoxesValues[0], 
+                onChanged: (bool? value) {
+                  setState(() {
+                    checkBoxesValues[0] = value;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+              CheckboxListTile(
+                title: Text('Feijão'),
+                value: checkBoxesValues[1], 
+                onChanged: (bool? value) {
+                  setState(() {
+                    checkBoxesValues[1] = value;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+              CheckboxListTile(
+                title: Text('Frango'),
+                value: checkBoxesValues[2], 
+                onChanged: (bool? value) {
+                  setState(() {
+                    checkBoxesValues[2] = value;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+              CheckboxListTile(
+                title: Text('Salada'),
+                value: checkBoxesValues[3], 
+                onChanged: (bool? value) {
+                  setState(() {
+                    checkBoxesValues[3] = value;
+                  });
+                },
+                controlAffinity: ListTileControlAffinity.leading,
+              ),
+              SizedBox(height: 10),
+              Text('Vai querer sobremesa?'),
+              RadioListTile(
+                title: Text('Sim'),
+                value: RadioBoxOptions.sim, 
+                groupValue: radioBoxGroup, 
+                onChanged: (RadioBoxOptions? value) {
+                  setState(() {
+                    radioBoxGroup = value;
+                  });
+                },
+              ),
+              RadioListTile(
+                title: Text('Não'),
+                value: RadioBoxOptions.nao, 
+                groupValue: radioBoxGroup, 
+                onChanged: (RadioBoxOptions? value) {
+                  setState(() {
+                    radioBoxGroup = value;
+                  });
+                },
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processando dados')),
+                    );
+                  }
+                },
+                child: const Text('Finalizar'),
+              ),
+            ],
           )
-        ],
+        )
       )
     );
   }
